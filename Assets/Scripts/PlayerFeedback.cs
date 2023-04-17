90c3d8e8e5ca1cb6e04ee2185e20501ac962ff80
Knoxulla <jyf20034@hotmail.com>
2023-04-17 19:55:32 -0400

    feedback

--- Assets/Scripts/PlayerFeedback.cs
+++ Assets/Scripts/PlayerFeedback.cs
@@ -7,16 +7,31 @@ public class PlayerFeedback : MonoBehaviour
 {
 
     public TMP_Text actionTxt;
+    public LayoutGroup charChoices;
     public bool flashOver = true;
 
     void Start()
     {
+        actionTxt.gameObject.SetActive(false);
+        charChoices.gameObject.SetActive(true);
         actionTxt = actionTxt.GetComponent<TMP_Text>();
     }
 
     public void WriteToScreen(string txt) 
     {
+        charChoices.gameObject.SetActive(false);
+        actionTxt.gameObject.SetActive(true);
         actionTxt.text = txt;
+        StartCoroutine(WaitForFeedback());
+        StopCoroutine(WaitForFeedback());
+
+    }
+
+    IEnumerator WaitForFeedback() 
+    {
+        yield return new WaitForSeconds(2);
+        actionTxt.gameObject.SetActive(false);
+        charChoices.gameObject.SetActive(true);
     }
 
     public IEnumerator FlashRed(Image im) 
