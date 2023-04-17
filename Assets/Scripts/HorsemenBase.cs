c7301f88fd4cdd30050851664c7327071713982d
Knoxulla <jyf20034@hotmail.com>
2023-04-17 14:23:06 -0400

    dodge added to dmg calc on base horsemen script

--- Assets/Scripts/HorsemenBase.cs
+++ Assets/Scripts/HorsemenBase.cs
@@ -7,7 +7,7 @@ public class HorsemenBase : Player
     // Inherit from Player (Player is actually health script)
 
     public bool playerTurnOver = false;
-    public bool playeerDodge = false;
+    public bool playerDodge = false;
     private System.Random rd = new System.Random();
     [SerializeField]
     Player Target;
@@ -24,13 +24,18 @@ public class HorsemenBase : Player
     {
         Target = Target.GetComponent<Player>();
     }
-    private int HitChance() 
+    private int HitChance()
     {
         int _attackHitChance = rd.Next(0, 100);
-        if (playeerDodge)
+        if (playerDodge)
         {
-            _attackHitChance = 0;
+            _attackHitChance -= rd.Next(1, 50);
+            if (_attackHitChance < 0)
+            {
+                _attackHitChance = 0;
+            }
         }
+        playerDodge = false;
         return _attackHitChance;
     }
 
@@ -39,15 +44,15 @@ public class HorsemenBase : Player
         int hitChance = HitChance();
         String _attackType = "";
 
-        if (hitChance >= 85)
+        if (hitChance >= 80)
         {
             _attackType = "HEAVY";
         }
-        else if (hitChance >= 50 && hitChance < 85)
+        else if (hitChance >= 40 && hitChance < 85)
         {
             _attackType = "NORMAL";
         }
-        else if (hitChance < 50 && hitChance >= 25)
+        else if (hitChance < 40 && hitChance >= 15)
         {
             _attackType = "WEAK";
         }
@@ -56,22 +61,19 @@ public class HorsemenBase : Player
         return _attackType;
     }
 
-    public void Attack() 
+    public void Attack()
     {
         String attack = AttackType();
-        if (attack.Equals("MISS"))
+
+        if (attack.Equals("HEAVY"))
         {
-            return;
-        }
-        else if (attack.Equals("HEAVY"))
-        {
-            Target.dmg(rd.Next(15,20)); // DMG = 15 - 20% player health (nums decided on how big health bar will be)
+            Target.dmg(rd.Next(15, 20)); // DMG = 15 - 20% player health (nums decided on how big health bar will be)
         }
         else if (attack.Equals("NORMAL"))
         {
             Target.dmg(rd.Next(8, 15)); // DMG = 8 - 15% player health (nums decided on how big health bar will be)
         }
-        else if (attack.Equals("WEAK")) 
+        else if (attack.Equals("WEAK"))
         {
             Target.dmg(rd.Next(3, 8)); // DMG = 3 - 8% player health (nums decided on how big health bar will be)
         }
@@ -80,11 +82,12 @@ public class HorsemenBase : Player
 
     private void Update()
     {
-        if (playerTurnOver) 
+        if (playerTurnOver)
         {
             Attack();
         }
         playerTurnOver = false;
+        
     }
 
 }
